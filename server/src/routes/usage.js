bb7ab8f05ce4f8a736c3b06b251b520314da6b32
Narendra Nareda <narendradmirer145@gmail.com>
2025-12-04 20:59:01 +0530

    path import fix

--- server/src/routes/usage.js
+++ server/src/routes/usage.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { authenticateToken } = require('../middleware/auth');
+const { authenticateToken } = require('../middleware/authMiddleware');
 const { getUsageWithLimits, getUsageHistory, resetUsage } = require('../services/usageService');
 const { getSubscription } = require('../services/subscriptionService');
 
